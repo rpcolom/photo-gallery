65a66c5231c00708622b95ac2656167f8f64f653
FI4 OPEN SOURCE <rpcolom@gmail.com>
2021-03-03 18:58:00 +0100

    Add updates

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+//import { PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
+import { Photo, PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  // constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService, 
+    public actionSheetController: ActionSheetController) {}  
 
-  constructor() {}
+  // constructor() {}
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+  
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }  
 
+  public async showActionSheet(photo: Photo, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }  
 }
